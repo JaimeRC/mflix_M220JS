43f9cce4112533f7c856b432840fbb0011cd2903
JaimeRC <jaime.rc@icloud.com>
2019-02-28 00:52:04 +0100

    Options Timeouts, Pool Size, handle errors and WriteConcern

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -32,7 +32,7 @@ export default class MoviesDAO {
     let response = {
       poolSize,
       wtimeout,
-      authInfo,
+      authInfo
     }
     return response
   }
--- src/index.js
+++ src/index.js
@@ -21,11 +21,11 @@ concern timeout limit to 2500 milliseconds.
 */
 MongoClient.connect(
   process.env.MFLIX_DB_URI,
-  // TODO: Connection Pooling
+  // TODO: Connection Pooling                     -----------------------------------------------> OK
   // Set the poolSize to 50 connections.
-  // TODO: Timeouts
+  // TODO: Timeouts                     -----------------------------------------------> OK
   // Set the write timeout limit to 2500 milliseconds.
-  { useNewUrlParser: true },
+  { poolSize: 50, wtimeout:2500,useNewUrlParser: true },
 )
   .catch(err => {
     console.error(err.stack)
